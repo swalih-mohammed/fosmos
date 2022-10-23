9b32891956c64d0809d39d9958e6d832fca1a6fd
Swalih  Mohammed <swalihmohammed@Swalihs-MacBook-Air.local>
2022-10-23 16:15:04 +0530

    first

---
+++ backend/members/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models import Member
+
+# Register your models here.
+class MemberAdmin(admin.ModelAdmin):
+    list_display = [
+        'first_name',  'last_name', 
+    ]
+    list_filter = ['education', ]
+    search_fields = ['first_name', 'last_name', "phone_number"]
+
+
+admin.site.register(Member, MemberAdmin)
---
+++ backend/members/models.py
@@ -0,0 +1,36 @@
+from random import choices
+from secrets import choice
+from django.db import models
+
+# Create your models here.
+
+EDUCATION_CHOICES = (
+    ("SSLC", "SSLC"),
+    ("+2", "+2"),
+    ("GRADUATION", "GRADUATION"),
+    ("PG", "PG"),
+    ("DOCTRATE", "DOCTRATE"),
+)
+PROFESSION_CHOICES = (
+    ("TEACHING", "TEACHING"),
+    ("IT", "IT"),
+    ("GULF", "GULF"),
+    ("OTHER", "OTHER"),
+)
+
+
+class Member(models.Model):
+    first_name = models.CharField(max_length=250, blank=True, null=True)
+    last_name = models.CharField(max_length=250, blank=True, null=True)
+    phone_number = models.CharField(max_length=250, blank=True, null=True)
+    email = models.CharField(max_length=250, blank=True, null=True)
+    education = models.CharField(max_length=250, blank=True, null=True,  choices=EDUCATION_CHOICES,  )
+    profession = models.CharField(max_length=250, blank=True, null=True,  choices=EDUCATION_CHOICES,  )
+    graduated = models.CharField(max_length=250, blank=True, null=True)
+    address = models.TextField(max_length=250, blank=True, null=True)
+
+    def __str__(self):
+        return self.first_name + " " + self.last_name
+
+    class Meta:
+        ordering = ['first_name']
